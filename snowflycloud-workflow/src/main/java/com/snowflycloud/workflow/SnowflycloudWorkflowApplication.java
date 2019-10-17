632cfb37bc8778473b585a36feb05bb5e5aaf1de
liqiaozi <2582376671@qq.com>
2019-10-17 21:46:21 +0800

    项目初始化

---
+++ snowflycloud-workflow/src/main/java/com/snowflycloud/workflow/SnowflycloudWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.snowflycloud.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowflycloudWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowflycloudWorkflowApplication.class, args);
+    }
+
+}
---
+++ snowflycloud-workflow/src/test/java/com/snowflycloud/workflow/SnowflycloudWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowflycloud.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowflycloudWorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
