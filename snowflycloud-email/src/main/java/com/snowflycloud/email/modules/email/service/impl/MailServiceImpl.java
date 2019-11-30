7ffd2e3e8bf8c0a59de13452a1050f3394136e6c
liqiaozi <2582376671@qq.com>
2019-12-01 00:09:29 +0800

    删减不必要的项目

--- snowflycloud-email/src/main/java/com/snowflycloud/email/modules/email/service/impl/MailServiceImpl.java
+++ snowflycloud-email/src/main/java/com/snowflycloud/email/modules/email/service/impl/MailServiceImpl.java
@@ -59,10 +59,10 @@ public class MailServiceImpl implements IMailService {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(javaMailSender.getUsername());
         message.setTo(request.getTo().toArray(new String[request.getTo().size()]));
-        if(!CollectionUtils.isEmpty(request.getCc())){
+        if (!CollectionUtils.isEmpty(request.getCc())) {
             message.setCc(request.getCc().toArray(new String[request.getCc().size()]));
         }
-        if(!CollectionUtils.isEmpty(request.getBcc())){
+        if (!CollectionUtils.isEmpty(request.getBcc())) {
             message.setBcc(request.getBcc().toArray(new String[request.getBcc().size()]));
         }
 
@@ -72,7 +72,7 @@ public class MailServiceImpl implements IMailService {
             javaMailSender.send(message);
             logger.info("[sendSimpleMail] ### 发送简单邮件成功....");
         } catch (Exception e) {
-            logger.error("[sendSimpleMail] ### 发送简单邮件异常....",e.getMessage());
+            logger.error("[sendSimpleMail] ### 发送简单邮件异常....", e.getMessage());
             e.printStackTrace();
         }
         return ResultResponse.ok();
@@ -82,14 +82,14 @@ public class MailServiceImpl implements IMailService {
     public ResultResponse sendHtmlMail(EmailRequest request) {
         JavaMailSenderImpl javaMailSender = emailSenderProcessor.createMailSender(request.getUserId(), request.getBusiness());
         MimeMessage message = mailSender.createMimeMessage();
-        try{
-            MimeMessageHelper helper = buildMimeMessageHelper( request, message);
+        try {
+            MimeMessageHelper helper = buildMimeMessageHelper(request, message);
             helper.setFrom(javaMailSender.getUsername());
-            helper.setText(request.getContent(),true);
+            helper.setText(request.getContent(), true);
             mailSender.send(message);
             logger.info("[sendHtmlMail] ### 发送Html邮件成功....");
-        }catch (Exception e){
-            logger.error("[sendHtmlMail] ### 发送Html邮件异常....",e.getMessage());
+        } catch (Exception e) {
+            logger.error("[sendHtmlMail] ### 发送Html邮件异常....", e.getMessage());
             e.printStackTrace();
         }
         return ResultResponse.ok();
@@ -99,19 +99,19 @@ public class MailServiceImpl implements IMailService {
     public ResultResponse sendAttachmentsMail(EmailRequest request) {
         JavaMailSenderImpl javaMailSender = emailSenderProcessor.createMailSender(request.getUserId(), request.getBusiness());
         MimeMessage message = mailSender.createMimeMessage();
-        try{
-            MimeMessageHelper helper = buildMimeMessageHelper( request, message);
+        try {
+            MimeMessageHelper helper = buildMimeMessageHelper(request, message);
             helper.setFrom(request.getFrom());
-            helper.setText(request.getContent(),true);
+            helper.setText(request.getContent(), true);
 
             // 处理附件
             Map<String, String> attachments = request.getAttachments();
-            if(attachments != null && attachments.size() > 0){
+            if (attachments != null && attachments.size() > 0) {
                 attachments.entrySet().iterator().forEachRemaining(item -> {
                     String attachmentName = item.getKey();
                     FileSystemResource file = new FileSystemResource(new File(item.getValue()));
                     try {
-                        helper.addAttachment(attachmentName,file);
+                        helper.addAttachment(attachmentName, file);
                     } catch (MessagingException e) {
                         logger.info("[sendAttachmentsMail] ### 处理附件异常....数据信息 = {}", JSONObject.toJSONString(item));
                         e.printStackTrace();
@@ -121,8 +121,8 @@ public class MailServiceImpl implements IMailService {
 
             mailSender.send(message);
             logger.info("[sendAttachmentsMail] ### 发送带附件邮件成功....");
-        }catch (Exception e){
-            logger.error("[sendAttachmentsMail] ### 发送带附件邮件异常....",e.getMessage());
+        } catch (Exception e) {
+            logger.error("[sendAttachmentsMail] ### 发送带附件邮件异常....", e.getMessage());
             e.printStackTrace();
         }
 
@@ -133,19 +133,19 @@ public class MailServiceImpl implements IMailService {
     public ResultResponse sendInlineResourceMail(EmailRequest request) {
         JavaMailSenderImpl javaMailSender = emailSenderProcessor.createMailSender(request.getUserId(), request.getBusiness());
         MimeMessage message = mailSender.createMimeMessage();
-        try{
-            MimeMessageHelper helper = buildMimeMessageHelper( request, message);
+        try {
+            MimeMessageHelper helper = buildMimeMessageHelper(request, message);
             helper.setFrom(javaMailSender.getUsername());
-            helper.setText(request.getContent(),true);
+            helper.setText(request.getContent(), true);
 
             // 处理附件
             Map<String, String> inlineResources = request.getInlineResources();
-            if(inlineResources != null && inlineResources.size() > 0){
+            if (inlineResources != null && inlineResources.size() > 0) {
                 inlineResources.entrySet().iterator().forEachRemaining(item -> {
                     String resourceId = item.getKey();
                     FileSystemResource file = new FileSystemResource(new File(item.getValue()));
                     try {
-                        helper.addAttachment(resourceId,file);
+                        helper.addAttachment(resourceId, file);
                     } catch (MessagingException e) {
                         logger.info("[sendInlineResourceMail] ### 处理嵌入静态资源的信息异常....数据信息 = {}", JSONObject.toJSONString(item));
                         e.printStackTrace();
@@ -155,8 +155,8 @@ public class MailServiceImpl implements IMailService {
 
             mailSender.send(message);
             logger.info("[sendInlineResourceMail] ### 发送带嵌入静态资源邮件成功....");
-        }catch (Exception e){
-            logger.error("[sendInlineResourceMail] ### 发送带嵌入静态资源邮件异常....",e.getMessage());
+        } catch (Exception e) {
+            logger.error("[sendInlineResourceMail] ### 发送带嵌入静态资源邮件异常....", e.getMessage());
             e.printStackTrace();
         }
 
@@ -169,7 +169,7 @@ public class MailServiceImpl implements IMailService {
         JavaMailSenderImpl javaMailSender = emailSenderProcessor.createMailSender(request.getUserId(), request.getBusiness());
         MimeMessage message = mailSender.createMimeMessage();
         try {
-            MimeMessageHelper helper = buildMimeMessageHelper( request, message);
+            MimeMessageHelper helper = buildMimeMessageHelper(request, message);
             helper.setFrom(javaMailSender.getUsername());
             Map<String, Object> variables = request.getTemplateVariables();
             String text = freemarkerTransformEngine.processTemplateToString(request.getTemplateKey(), variables);
@@ -177,7 +177,7 @@ public class MailServiceImpl implements IMailService {
             mailSender.send(message);
 
         } catch (Exception e) {
-            logger.error("[sendInlineResourceMail] ### 发送模板邮件异常....",e.getMessage());
+            logger.error("[sendInlineResourceMail] ### 发送模板邮件异常....", e.getMessage());
             e.printStackTrace();
         }
 
@@ -187,17 +187,18 @@ public class MailServiceImpl implements IMailService {
 
     /**
      * 组装参数
+     *
      * @param request
      * @param message
      * @throws MessagingException
      */
-    private MimeMessageHelper buildMimeMessageHelper(EmailRequest request, MimeMessage message ) throws MessagingException {
+    private MimeMessageHelper buildMimeMessageHelper(EmailRequest request, MimeMessage message) throws MessagingException {
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
         helper.setTo(request.getTo().toArray(new String[request.getTo().size()]));
-        if(!CollectionUtils.isEmpty(request.getCc())){
+        if (!CollectionUtils.isEmpty(request.getCc())) {
             helper.setCc(request.getCc().toArray(new String[request.getCc().size()]));
         }
-        if(!CollectionUtils.isEmpty(request.getBcc())){
+        if (!CollectionUtils.isEmpty(request.getBcc())) {
             helper.setBcc(request.getBcc().toArray(new String[request.getBcc().size()]));
         }
         helper.setSubject(request.getSubject());
